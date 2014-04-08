7756f60b16b731ecff5e60ec49d5dd9857373cbb
Lydia Beaudrot <lbeaudrot@conservation.org>
2014-04-08 10:17:50 -0700

    clean genus names for lianas

--- PlantDiversity.R
+++ PlantDiversity.R
@@ -6,7 +6,11 @@
 
 
 # Query vegetation data from database
+#Vdata <- f.teamdb.query("vegetation")
+#Vegdata <- Vdata
 
-Vdata <- f.teamdb.query("vegetation")
-Vegdata <- Vdata
-
+# Clean genus names for trees and lianas
+#Vdataclean <- table(Vegdata$tree$Genus)
+#write.table(Vdataclean, file="Vdataclean.csv", sep=",")
+#Vdataclean.liana <- table(Vegdata$liana$Genus)
+#write.table(Vdataclean.liana, file="Vdataclean.liana.csv", sep=",")
