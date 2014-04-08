8377beb129f44c0ca28f1850c2569e869da6582e
Lydia Beaudrot <lbeaudrot@conservation.org>
2014-04-08 06:20:34 -0700

    pull all plant data for diversity metrics

---
+++ PlantDiversity.R
@@ -0,0 +1,12 @@
+# Calculate plant metrics for plants-animals project
+# Above Ground Biomass
+# Species Richness
+# Species Diversity
+# Functional Diversity 
+
+
+# Query vegetation data from database
+
+Vdata <- f.teamdb.query("vegetation")
+Vegdata <- Vdata
+
