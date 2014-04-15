757371729212a86a96b88a77ea591c16462d4a7e
Lydia Beaudrot <lbeaudrot@conservation.org>
2014-04-15 08:18:31 -0700

    set calc.FRic=FALSE for dbFD to loop all sites
    this removes calculations of FRic and FDiv

--- PlantDiversity.R
+++ PlantDiversity.R
@@ -351,7 +351,7 @@ for(i in 1:length(levels(Vtraits$Site.CodeT))){
 
 FDsites <- list()
 for(i in 1:length(VSites)){
-  FDsites[[i]] <- dbFD(x=as.data.frame(VSites[[i]]), corr="cailliez")
+  FDsites[[i]] <- dbFD(x=as.data.frame(VSites[[i]]), corr="cailliez", calc.FRic=FALSE)
   #names(FDsites)[[i]] <- print(paste(names(VSites[[i]]))
      FDsites                          
 }
