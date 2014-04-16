71eb8a81cbf0677c315e095f753da2929fa3c980
Lydia Beaudrot <lbeaudrot@conservation.org>
2014-04-16 13:29:14 -0700

    commit

--- PlantDiversity.R
+++ PlantDiversity.R
@@ -211,6 +211,10 @@ Trees <- Trees[is.na(Trees$Id)==FALSE,]
 aliveT <- grep(pattern="K", x=Trees$ConditionCodes, invert=TRUE)
 Trees <- Trees[aliveT,]
 
+# Add decimal KRP plot with inflated diameters until issue is resolved in the database
+Trees$Diameter[Trees$"1haPlotNumber"=="VG-KRP-1"] <- Trees$Diameter[Trees$"1haPlotNumber"=="VG-KRP-1"]/10
+
+
 Lianas <-  rbind(Vlianas[Vlianas$Site.CodeL=="CAX" & Vlianas$SamplingPeriod=="2012.01",],
                  Vlianas[Vlianas$Site.CodeL=="YAS" & Vlianas$SamplingPeriod=="2012.01",],
                  Vlianas[Vlianas$Site.CodeL=="PSH" & Vlianas$SamplingPeriod=="2012.01",],
